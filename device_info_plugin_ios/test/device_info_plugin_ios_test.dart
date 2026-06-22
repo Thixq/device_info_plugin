6b9826112fb8466f155c8be505ff92c036a9e555
Kaan <kaefean1@gmail.com>
2026-06-22 23:17:34 +0300

    feat: initialize device_info_plugin_ios package structure

---
+++ device_info_plugin_ios/test/device_info_plugin_ios_test.dart
@@ -0,0 +1,16 @@
+import 'package:device_info_plugin_ios/device_info_plugin_ios.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
