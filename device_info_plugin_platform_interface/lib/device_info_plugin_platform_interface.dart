7bde6b6e2af5480045f75c4b7ec07670e8a095eb
Kaan <kaefean1@gmail.com>
2026-06-23 23:34:48 +0300

    feat(platform_interface): setup federated interface, tests, and models

--- device_info_plugin_platform_interface/lib/device_info_plugin_platform_interface.dart
+++ device_info_plugin_platform_interface/lib/device_info_plugin_platform_interface.dart
@@ -1,5 +1,2 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/device_info_model.dart';
+export 'src/device_info_plugin_platform.dart';
